82b2ddcb204627c83aa10b414ee100622e066918
Krishnaswamipillai <36609892+Krishnaswamipillai@users.noreply.github.com>
2018-02-24 13:47:39 -0800

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,36 @@
+from random import randint
+
+def make_fair_dice(sides):
+    """Returns a die that returns 1 to SIDES with equal chance."""
+    assert type(sides) == int and sides >= 1, 'Illegal value for sides'
+    def dice():
+        return randint(1,sides)
+    return dice
+
+four_sided = make_fair_dice(4)
+six_sided = make_fair_dice(6)
+
+def make_test_dice(*outcomes):
+    """Returns a die that cycles deterministically through OUTCOMES.
+
+    >>> dice = make_test_dice(1, 2, 3)
+    >>> dice()
+    1
+    >>> dice()
+    2
+    >>> dice()
+    3
+    >>> dice()
+    1
+    >>> dice()
+    2
+    """
+    assert len(outcomes) > 0, 'Outcomes must be supplied to make_test_dice'
+    for o in outcomes:
+        assert type(o) == int and o >= 1, 'Outcome is not a positive integer'
+    index = len(outcomes) - 1
+    def dice():
+        nonlocal index
+        index = (index + 1) % len(outcomes)
+        return outcomes[index]
+    return dice
